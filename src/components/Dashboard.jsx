9baa5dc2847e7f9f7d49c5a5b27eca2647a55ba8
NABATIVIJAY <vijaymuruganm262000@gmail.com>
2024-03-12 01:02:51 +0530

    react project

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -124,6 +124,13 @@ function Dashboard() {
 </div>
 
 
+<div className="container-fluid">
+   <div className="d-sm-flex align-items-center justify-content-between mb-4">
+             <h1 className="h3 mb-0 text-gray-800">Terms & Conditions</h1>
+             
+</div>
+</div>
+
 </div>
 </>
 }
